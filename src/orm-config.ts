6ff42f7ea0b9bab2d3300409b94d4a1c85d8b2d5
lucas <lucasroncon16@gmail.com>
2025-04-13 17:13:27 -0300

    iniacializando projeto

---
+++ src/orm-config.ts
@@ -0,0 +1,9 @@
+import { DataSourceOptions } from 'typeorm';
+
+export const config: DataSourceOptions = {
+  type: 'sqlite',
+  database: '.db/sql',
+  synchronize: true, 
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+};
+
